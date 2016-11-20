f444f5a833857f5bcfab2e75b52d8b352077ac3a
seochangwook <scw05313315@gmail.com>
2016-11-20 18:17:32 +0900

    test

--- sample_3.js
+++ sample_3.js
@@ -1,9 +1,9 @@
-//Simple Web server//
-var http = require('http'); //http의 자원을 얻어온다.//
-var os_info = require('os'); //os의 자원을 얻어온다.//
+//모듈//
+var http = require('http');
 
-var os_platform_info = os_info.platform();
+var a = [3,1,2];
 
+//네트워크 관련 변수//
 var hostname = '127.0.0.1';
 var port = 1337;
 
@@ -12,9 +12,18 @@ http.createServer((req, res) =>
 {
     res.writeHead(200, {'Content-Type' : 'text/plain'}); //헤더작성//
     //res.end('Hello World nodejs\n');
-    res.end('os platform : [' + os_platform_info + ']');
+    Sort_func(res, a); //함수를 호출//
 }).listen(port, hostname, () => //listen으로 개방상태, 즉 클라이언트가 접속하는것을 가능.//
 {
     //node에서는 `, ' 는 다르다.//
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+//////////////////
+function Sort_func(res, number_array)
+{
+    number_array.sort();
+
+    console.log(a);
+
+    res.end(a+'\n');
+}
