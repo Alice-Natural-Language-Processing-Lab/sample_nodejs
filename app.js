95c91518f31dde105ddaa7cddf360ad1388e5a0c
seochangwook <scw05313315@gmail.com>
2016-11-23 21:44:11 +0900

    test(router, module)

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var app = express();
 //라우터별로 분리하기 위해 express의 라우터 기능 사용//
 
 /* 
-(/function) :
+(/function) 라우터에 위임된 항목 :
+ex) /function/~
 1. function/file_upload.js
 2. function/push.js
 */
-//사용자 정의 모듈 호출//
+//사용자 정의 모듈 호출(모듈경로)//
 var function_fileupload_router = require('./function_fileupload/file_upload');
-app.use('/function', function_fileupload_router); //'/file_upload'에 대한 명령 시 라우터에게 위임//
+app.use('/function', function_fileupload_router); 
 
 var function_push_router = require('./function_push/push');
 app.use('/function', function_push_router);
