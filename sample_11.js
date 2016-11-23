c14bc7c5f8d65f3d83ca1f0a48fd583394114da0
seochangwook <scw05313315@gmail.com>
2016-11-23 16:31:00 +0900

    test(multipart/form-data)

--- sample_11.js
+++ sample_11.js
@@ -9,19 +9,19 @@ var app = express();
 //POST설정//
 app.use(bodyParser.json());
 
-var form = new formidable.IncomingForm();
-//업로드 정보(인코딩, 저장 디렉터리) 설정//
-form.encoding = 'utf-8'; //인코딩 타입 정의//
-form.uploadDir = '/Users/apple/Desktop/Programmingfile/nodejs/sample_nodejs/upload_file_folder'; //저장 디렉터리 지정//
-form.multiples = true; //request.files to be arrays of files//
-form.keepExtensions = true; //확장자 표시//
-
 //받는 변수//
 var fields = new Array();
 var files = new Array();
 
 //기본 post방식으로 전송//
 app.post('/file_upload', function(request, response){
+    var form = new formidable.IncomingForm(); //헤더를 만들어주는 역할이기에 밖에 있으면 안된다.//
+    //업로드 정보(인코딩, 저장 디렉터리) 설정//
+    form.encoding = 'utf-8'; //인코딩 타입 정의//
+    form.uploadDir = '/Users/apple/Desktop/Programmingfile/nodejs/sample_nodejs/upload_file_folder'; //저장 디렉터리 지정//
+    form.multiples = true; //request.files to be arrays of files//
+    form.keepExtensions = true; //확장자 표시//
+
     //form타입 필드(text타입)에 따른 이벤트(콜백 방식으로 구성)//
     form.on('field', function(field, value){
         //console.log('[field]' + field, value);
@@ -32,12 +32,6 @@ app.post('/file_upload', function(request, response){
         fs.rename(file.path, form.uploadDir+ '/' + file.name); //파일의 이름 변경//
         files.push([field, file.name]);
     }).on('end', function() {
-        //console.log('-> upload done');
-        //response.writeHead(200, {'content-type': 'text/plain'});
-        //response.write('received fields:\n\n '+util.inspect(fields));
-        //response.write('\n\n');
-        //response.end('received files:\n\n '+util.inspect(files));
-
         console.log('----------<fields>----------');
         var field_json = JSON.stringify(fields); //string으로 반환//
         console.log(field_json);
@@ -55,13 +49,15 @@ app.post('/file_upload', function(request, response){
 
         var trans_objeect = 
         {
-            'result':result
+            'is_upload':'success',
+            'data': result
         }
 
         response.send(trans_objeect);
 
         fields = [];
         files = [];
+        
     }).on('error', function(error) {
         console.log('[error] error : ' + error);
     });
