5bae1b9cce07102adb2c6d283c40f8dc84743193
seochangwook <scw05313315@gmail.com>
2016-12-17 14:50:41 +0900

    crypto(aes256, sha512)

--- app_crypto.js
+++ app_crypto.js
@@ -21,33 +21,17 @@ var crypto_inputtype = 'utf8';
 var crypto_outputtype = 'hex';
 // ********************************* //
 
-// ******<<Hash 암호화 관련 변수******** //
-var hash_algorithm = 'sha256'; //일반적으로 sha-256, sha-512를 많이 사용//
-var hash_key = 'HASH_KEY';
-var hash_outputtype = 'hex';
-/*눈사태 효과 : 원문(Plaintext)의 한 비트의 변화가 최종 암호문(Ciphertext)에 큰 변화를 주는 효과.모든 암호에서 핵심적으로
-요구되는 암호학적 특징. 특히 블록암호나 단방향 해시 함수에서 주로 요구한다. 조금만 바뀌어도 유사성을 찾을 수 없게 함. 암호를 만들
-때 모든 경우의 수를 다 계산해보지 않는 이상 결과를 알 수 있다는 보장을 할 수 없게 해야 하며 이를 위해 눈사태 효과 개념을 도입.
-대표적인 알고리즘들로는 AES, SHA-1, SHA-2, MD5가 있다.*/
-// ********************************* //
-
 //암호화 모듈 서비스//
 var cipher = crypto.getCiphers(); //Cipher방식의 암호화 알고리즘의 정보를 출력//
 /*Cipher기법은 일반적인 암호화 방식으로 평문 -> 암호문 -> 복호문(평문)의 방식으로 암/복호화를 합니다.//
 즉 암호화를 하기 위한 key를 가지고 평문을 암호화하고, 그것을 사용할 때에는 다시 Key를 통해 복호화하여 사용*/
 
-var hash = crypto.getHashes(); //Hash방식의 암호화 알고리즘 정보 출력//
-/*Hash기법은 복호화가 불가하다. 일반적으로 유효검증(Validation)을 이용할 때 많이 사용된다. 데이터베이스에 저장
-시 중요 검증되어야 할 정보들은 해시로 암호화한 후 저장. 중요 정보 관리 시 RDBMS를 사용 */
-//console.log(cipher); //암호화 알고리즘 종류를 출력//
-//console.log(hash);
-
 //입력변수//
 var cipherd; //aes로 암호화 된 암호문//
-var original_text; //해시가 적용되지 않은 일반 평문//
+var hash_encrypt_data; //해시가 적용된 암호문//
 
 //적용 가능 알고리즘 목록//
-var algorithm_list = [{'name':'aes256'}, {'name':'sha256'}];
+var algorithm_list = [{'name':'aes128'}, {'name':'sha512'}];
 
 app.post('/cipher_test', function(request, response){
     cipherd = request.body.input_password; //기존 약속된 암호화 방식으로 암호화된 데이터가 입력//
@@ -55,10 +39,10 @@ app.post('/cipher_test', function(request, response){
     Decrypt_str(cipherd, response); //
 });
 
-app.post('/hash_test', function(request, response){
-    original_text = request.body.input_str; //해시가 적용되어야 할 일반 평문 데이터//
+app.post('/hash_trans', function(request, response){
+    hash_encrypt_data = request.body.input_str; //해시가 적용되어야 할 일반 평문 데이터//
 
-    Encrypt_hash(original_text, response);
+    save_hash(hash_encrypt_data, response);
 });
 
 app.get('/algorithm_list', function(request, response){
@@ -105,11 +89,14 @@ function Decrypt_str(cipherd, response){
     response.send(trans_json);
 }
 //////////////////////////
-function Encrypt_hash(original_text, response){
-    var hmac = crypto.createHmac(hash_algorithm, hash_key);
-    var pass = hmac.update(original_text).digest(hash_outputtype);
+function save_hash(hash_encrypt_data, response){
+    console.log('Encrypt hash str : ' + hash_encrypt_data);
 
-    console.log('Encrypt hash str : ' + pass);
+    /*눈사태 효과 : 원문(Plaintext)의 한 비트의 변화가 최종 암호문(Ciphertext)에 큰 변화를 주는 효과.모든 암호에서 핵심적으로
+    요구되는 암호학적 특징. 특히 블록암호나 단방향 해시 함수에서 주로 요구한다. 조금만 바뀌어도 유사성을 찾을 수 없게 함. 암호를 만들
+    때 모든 경우의 수를 다 계산해보지 않는 이상 결과를 알 수 있다는 보장을 할 수 없게 해야 하며 이를 위해 눈사태 효과 개념을 도입.
+    대표적인 알고리즘들로는 AES, SHA-1, SHA-2, MD5가 있다. 사용자가 중요 정보의 값을 해시로 전달해서 저장하게 되므로 개발자도 값
+    을 알 수 가 없게 된다. 또한 추후 해당 값을 데이터베이스로부터 다시 전송하여 비교함으로서 무결성검사도 할 수 있게 된다.*/
 
     //암호화된 값을 활용//
     console.log('--------------------------');
@@ -117,7 +104,7 @@ function Encrypt_hash(original_text, response){
     //전송할 JSON포맷을 만든다.//
     var result = 
     {
-        'encrypt':pass
+        'encrypt':hash_encrypt_data
     }
 
     var trans_objeect = 
